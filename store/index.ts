62a8753726021fcd65c09130ad4c7e796ae01cc7
Donald Tran <yosoup@outlook.com>
2022-09-12 11:36:07 -0700

    Alias `useDispatch` and `useSelector` with typed versions

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,13 @@
-import {
-    configureStore,
-} from '@reduxjs/toolkit';
-import appReducer from './slices/app';
+import { configureStore } from "@reduxjs/toolkit";
+import appReducer from "./slices/app";
 
-export default configureStore({
-    reducer: {
-      app: appReducer,
-    },
+const store = configureStore({
+  reducer: {
+    app: appReducer,
+  },
 });
+
+export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
---
+++ store/slices/types.ts
@@ -0,0 +1,9 @@
+/**
+ * This is typing for the returns of `createAsyncThunk`
+ * as well as being slightly friendlier names
+ */
+export enum AsyncStatus {
+  IDLE = "IDLE",
+  LOADING = "LOADING",
+  FAILED = "FAILED",
+}
