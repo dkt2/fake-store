35adf685833b0857a16981376549865908d425f0
Donald Tran <yosoup@outlook.com>
2022-09-12 12:22:17 -0700

    Hide product price if > $500 and not logged in

--- components/ProductTile/ProductTile.tsx
+++ components/ProductTile/ProductTile.tsx
@@ -1,30 +1,31 @@
-import Image from 'next/image';
-import React from 'react';
-import formatPrice from 'util/formatPrice';
-import classes from './ProductTile.module.css';
+import Image from "next/image";
+import React from "react";
+import { useAppSelector } from "store/hooks";
+import { selectIsLoggedIn } from "store/slices/app";
+import formatPrice from "util/formatPrice";
+import classes from "./ProductTile.module.css";
 
 interface ProductTileProps {
-    product: any;
+  product: any;
 }
 
 function ProductTile(props: ProductTileProps) {
-    const { product } = props;
-
-    return (
-        <div className={classes.productTile}>
-            <picture className={classes.productImage}>
-                <source srcSet={product.image} type={'image/webp'} />
-                <img src={product.image} alt={'Product Photo'} />
-            </picture>
-            <div className={classes.productTitle}>
-                {product?.title}
-            </div>
-            <div className={classes.productPrice}>
-                {formatPrice(product.price)}
-            </div>
-            
-        </div>
-    )
+  const { product } = props;
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  return (
+    <div className={classes.productTile}>
+      <picture className={classes.productImage}>
+        <source srcSet={product.image} type={"image/webp"} />
+        <img src={product.image} alt={"Product Photo"} />
+      </picture>
+      <div className={classes.productTitle}>{product?.title}</div>
+      <div className={classes.productPrice}>
+        {isLoggedIn || product.price < 500
+          ? formatPrice(product.price)
+          : "Sign in to view pricing"}
+      </div>
+    </div>
+  );
 }
 
 export default ProductTile;
