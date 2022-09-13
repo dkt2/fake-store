e69207d08335ba3aa5c06bd73d0c29db38a8d2e2
Donald Tran <yosoup@outlook.com>
2022-09-12 23:31:49 -0700

    store/slices/types: edit comment

--- store/slices/types.ts
+++ store/slices/types.ts
@@ -1,6 +1,6 @@
 /**
  * This is typing for the returns of `createAsyncThunk`
- * as well as being slightly friendlier names
+ *  as well as being slightly friendlier names
  */
 export enum AsyncStatus {
   IDLE = "IDLE",
