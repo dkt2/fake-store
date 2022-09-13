3bf2a7fa21440aae7b82420e43f5db35d1cd9b96
Donald Tran <yosoup@outlook.com>
2022-09-12 22:38:22 -0700

    Add Shopping Cart link to Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,26 +1,30 @@
-import CategoryMenu from '../CategoryMenu/CategoryMenu';
-import Image from 'next/image';
-import classes from './Header.module.css';
-import Link from 'next/link';
-import AccountButton from '../AccountButton';
+import CategoryMenu from "../CategoryMenu/CategoryMenu";
+import Image from "next/image";
+import classes from "./Header.module.css";
+import Link from "next/link";
+import AccountButton from "../AccountButton";
+import ShoppingCartLink from "../ShoppingCart/ShoppingCartLink";
 
 function Header() {
-    return (
-        <div className={classes.siteHeader}>
-            <div className={classes.left}>
-                <div className={classes.siteLogo}>
-                    <Link href={'/'}>
-                        <Image src={require('../../assets/fakestorelogo.png')} alt={'Website logo'}/>
-                    </Link>
-                </div>
-                <CategoryMenu />
-            </div>
-            <div className={classes.right}>
-                <AccountButton />
-            </div>
-            
+  return (
+    <div className={classes.siteHeader}>
+      <div className={classes.left}>
+        <div className={classes.siteLogo}>
+          <Link href={"/"}>
+            <Image
+              src={require("../../assets/fakestorelogo.png")}
+              alt={"Website logo"}
+            />
+          </Link>
         </div>
-    )
+        <CategoryMenu />
+      </div>
+      <div className={classes.right}>
+        <ShoppingCartLink />
+        <AccountButton />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
