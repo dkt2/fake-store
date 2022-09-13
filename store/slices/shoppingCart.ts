2ec3b31fd3d1852099ba2b560a81d4eaf500c9c0
Donald Tran <yosoup@outlook.com>
2022-09-13 00:11:41 -0700

    store/slice/shoppingCart: add comments

--- store/slices/shoppingCart.ts
+++ store/slices/shoppingCart.ts
@@ -2,6 +2,11 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "store";
 import { AsyncStatus } from "./types";
 
+/**
+ * Unless otherwise noted,
+ *  these types are to type
+ *  the return values of external APIs
+ */
 type ProductFromAPI = {
   id: number;
   title: string;
@@ -19,6 +24,10 @@ type ProductFromCart = {
   quantity: number;
 };
 
+/**
+ * This type represents all the fields we need
+ *  in order to display Product (ex. in ProductTile)
+ */
 export type Product = {
   image: string;
   title: string;
@@ -72,19 +81,44 @@ async function getProduct(
     .then((data: ProductFromAPI) => ({ ...data, quantity: product.quantity }));
 }
 
+/**
+ * `createAsyncThunk` wraps Async work with states that
+ *  can be selected on to show UI tracking the progress of the Async work.
+ * Reference `extraReducer` below for more info.
+ */
 export const getCurrentUserShoppingCarts = createAsyncThunk(
   "shoppingCart/getCurrentUserShoppingCarts",
   async (payload: { username: string }) => {
     const { username: currentUsername } = payload;
+
+    /**
+     * The login API doesn't give us anything to identify the logged in user with.
+     * So we have to find it based off of the username they give in the login form.
+     */
     const users: User[] = await fetch("https://fakestoreapi.com/users").then(
       (res) => res.json()
     );
+
+    /**
+     * This is where we find the current user;
+     *  we get the username to search for from the payload
+     */
     const currentUser = users.find((user) => user.username === currentUsername);
 
+    /**
+     * Once we have the current user,
+     *  we use that to query for their shopping carts.
+     */
     const cartsUnprocessed: CartUnprocessed[] = await fetch(
       `https://fakestoreapi.com/carts/user/${currentUser.id}`
     ).then((res) => res.json());
 
+    /**
+     * Since the fields in the products returned from the carts API
+     *  is different from the fields in the products we need for displaying products,
+     *  we have to do an additional step of getting the products info we need
+     *  through another API call
+     */
     const carts: Cart[] = await Promise.all(
       cartsUnprocessed.map(
         async (cart): Promise<Cart> => ({
@@ -97,6 +131,7 @@ export const getCurrentUserShoppingCarts = createAsyncThunk(
       )
     );
 
+    // This value becomes the `fulfilled` action payload
     return carts;
   }
 );
@@ -108,9 +143,27 @@ export const shoppingCartSlice = createSlice({
   name: "shoppingCart",
   initialState,
   reducers: {
+    /**
+     * setCart to null to clear the currently selected cart
+     */
     setCart: (state, action: PayloadAction<number>) => {
       state.currentCartID = action.payload;
     },
+    /**
+     * upsert = update + insert
+     *
+     * For adding/inserting:
+     *  Before adding a product to the cart,
+     *    it first checks if the product is already in the cart
+     *    and increments its quantity if it exists
+     *    otherwise it adds it with a quantity of 1
+     *
+     * For updating:
+     *  Samething as adding/inserting, but
+     *    without adding a product to the cart
+     *    and it should replace the quantity with a new value.
+     *  Set the `replace` field to `true` if using it for this use case
+     */
     upsertProduct: (
       state,
       action: PayloadAction<{ product: Product; replace?: boolean }>
@@ -140,6 +193,11 @@ export const shoppingCartSlice = createSlice({
     },
   },
   extraReducers(builder) {
+    /**
+     * These extra reducers are for displaying UI to track API progress.
+     * You can get the status of a `fetch`
+     *  by making a selector on `state.shoppingCart.status`.
+     */
     builder
       .addCase(getCurrentUserShoppingCarts.pending, (state) => {
         state.status = AsyncStatus.LOADING;
