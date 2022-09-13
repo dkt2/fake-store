9eb77e19595b8aa1a3d71ff5ff072d9f5898a9a1
Donald Tran <yosoup@outlook.com>
2022-09-12 22:45:23 -0700

    Implement Shopping Cart comp and add to ProductTile

--- components/ProductTile/ProductTile.tsx
+++ components/ProductTile/ProductTile.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { useAppSelector } from "store/hooks";
 import { selectIsLoggedIn } from "store/slices/app";
+import { Product } from "store/slices/shoppingCart";
 import formatPrice from "util/formatPrice";
+import ShoppingCart from "../ShoppingCart/ShoppingCart";
 import classes from "./ProductTile.module.css";
 
 interface ProductTileProps {
-  product: any;
+  product: Product;
 }
 
 function ProductTile(props: ProductTileProps) {
   const { product } = props;
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
+
   return (
     <div className={classes.productTile}>
       <picture className={classes.productImage}>
@@ -24,6 +27,7 @@ function ProductTile(props: ProductTileProps) {
           ? formatPrice(product.price)
           : "Sign in to view pricing"}
       </div>
+      {isLoggedIn && <ShoppingCart product={product} />}
     </div>
   );
 }
